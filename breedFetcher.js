d6911438d3e90beeac5d4a3fde09b4590c906174
Aidan Prior <sifiwizard.code@gmail.com>
2022-06-09 19:59:37 -0600

    Refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,15 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.error('error:', error);
-  }
-  const data = JSON.parse(body);
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log("Breed Not Found");
-  }
-});
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
